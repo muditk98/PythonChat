e7a6c746e45fe315dca876c30ae86dda8a879667
TheWussPuss <kapsmudit@gmail.com>
2018-10-14 17:19:38 +0530

    Initial Commit

---
+++ db_specs.py
@@ -0,0 +1,28 @@
+message {
+	id,
+	type,
+	data
+}
+
+users {
+	user_id,
+	user_name,
+	hash
+}
+
+user_ip {
+	user_id,
+	user_ip
+}
+
+group {
+	group_id,
+	group_name
+}
+
+group_user {
+	group_id,
+	user_id
+}
+
+
